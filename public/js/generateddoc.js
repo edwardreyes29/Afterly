c541d1e6779c039847c59f68eaf309ed1ad0cf6f
Edward Reyes <reyes.edwardf@gmail.com>
2020-09-11 11:09:31 -0700

    generate doc

--- public/js/generateddoc.js
+++ public/js/generateddoc.js
@@ -25,37 +25,45 @@ $(document).ready(() => {
     // =======================================
     // const tableNames = ['Hospice', 'Lawyers', 'Funeral', 'LifeInsurance'];
     const tableNames = ["lawyers", "insurances", "funerals", "hospices"];
-
+    const titleNames = ["Estate Planning", "Life Insurance", "Mortuary", "Hospice"]
+    const icons = ["balance scale icon", "money bill alternate outline icon", "cloud upload icon", "heartbeat icon"]
     const url_string = window.location.href;
     const url = new URL(url_string);
     const id = url.searchParams.get("caseId")
     const zip = url.searchParams.get("zipCode");
 
-    const createDoc = table => {
+    const createDoc = (table, title, icon) => {
+        console.log(icon);
         $.get(`/api/cases/${table}/${id}`).then(data => {
             console.log(data);
-            console.log(data.location)
-            // $("#doc-create-div").append(
-            //     `<div class="four wide column center aligned">
-            //         <div class="ui raised card">
-            //         <div class="content">
-            //             <div class="header">Estate Planning</div>
-            //             <div class="meta">
-            //             <span class="category estateName">${data.name}</span>
-            //             </div>
-            //             <div class="description">
-            //             <p class="estateAddress">Address goes Here</p>
-            //             </div>
-            //         </div>
-            //         <div class="extra content">
-            //             <div class="right floated author">
-            //             <i class="balance scale icon"></i>
-            //             </div>
-            //         </div>
-            //         </div>
-            //     </div>`
-            // )
+            console.log(data[0].location)
+            jsonLocation = JSON.parse(data[0].location);
+            $("#doc-create-div").append(
+                `<div class="four wide column center aligned">
+                    <div class="ui raised card">
+                    <div class="content">
+                        <div class="header">${title}</div>
+                        <div class="meta">
+                        <span class="category estateName">${data[0].name}</span>
+                        </div>
+                        <div class="description">
+                        <p class="estateAddress">${jsonLocation.display_address}</p>
+                        </div>
+                    </div>
+                    <div class="extra content">
+                        <div class="right floated author">
+                        <i class="${icon}"></i>
+                        </div>
+                    </div>
+                    </div>
+                </div>`
+            )
         })
+    }
+    // createDoc(tableNames[0]);
+    // tableNames.forEach(tableName => createDoc(tableName));
+    for (let i = 0; i < tableNames.length; i++) {
+        createDoc(tableNames[i], titleNames[i], icons[i])
     }
 
 }); // end document.ready
