9ea0f9a878e9e639a3ed03c138ebe8fa0f312f8a
Edward Reyes <reyes.edwardf@gmail.com>
2020-09-11 10:45:09 -0700

    hello

--- public/js/generateddoc.js
+++ public/js/generateddoc.js
@@ -19,3 +19,44 @@ function highlight() {
 function unhighlight() {
     dropzone.style("background-color", "#fff");
 }
+
+$(document).ready(() => {
+    // Global variables
+    // =======================================
+    // const tableNames = ['Hospice', 'Lawyers', 'Funeral', 'LifeInsurance'];
+    const tableNames = ["lawyers", "insurances", "funerals", "hospices"];
+
+    const url_string = window.location.href;
+    const url = new URL(url_string);
+    const id = url.searchParams.get("caseId")
+    const zip = url.searchParams.get("zipCode");
+
+    const createDoc = table => {
+        $.get(`/api/cases/${table}/${id}`).then(data => {
+            console.log(data);
+            console.log(data.location)
+            // $("#doc-create-div").append(
+            //     `<div class="four wide column center aligned">
+            //         <div class="ui raised card">
+            //         <div class="content">
+            //             <div class="header">Estate Planning</div>
+            //             <div class="meta">
+            //             <span class="category estateName">${data.name}</span>
+            //             </div>
+            //             <div class="description">
+            //             <p class="estateAddress">Address goes Here</p>
+            //             </div>
+            //         </div>
+            //         <div class="extra content">
+            //             <div class="right floated author">
+            //             <i class="balance scale icon"></i>
+            //             </div>
+            //         </div>
+            //         </div>
+            //     </div>`
+            // )
+        })
+    }
+
+}); // end document.ready
+
